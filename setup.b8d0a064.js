523305c55fb90766c5def58e8d1015f955bfb7fc
jakehl <jakelangford@outlook.com>
2020-10-18 23:58:59 +0100

    Updates

--- setup.b8d0a064.js
+++ setup.b8d0a064.js
@@ -30586,6 +30586,149 @@ var __assign = void 0 && (void 0).__assign || function () {
   return __assign.apply(this, arguments);
 };
 
+var __awaiter = void 0 && (void 0).__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
 var __rest = void 0 && (void 0).__rest || function (s, e) {
   var t = {};
 
@@ -30604,36 +30747,58 @@ var Camera = _react.default.forwardRef(function (_a, ref) {
       props = __rest(_a, ["deviceId"]);
 
   var videoRef = ref !== null && ref !== void 0 ? ref : (0, _react.useRef)();
+  var stream = (0, _react.useRef)();
 
-  var _b = (0, _react.useState)(),
-      stream = _b[0],
-      setStream = _b[1];
+  var setUp = function setUp() {
+    return __awaiter(void 0, void 0, void 0, function () {
+      var s;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            if (stream) tearDown();
+            return [4
+            /*yield*/
+            , navigator.mediaDevices.getUserMedia({
+              audio: false,
+              video: {
+                width: {
+                  exact: 128
+                },
+                height: {
+                  exact: 128
+                },
+                deviceId: deviceId
+              }
+            })];
+
+          case 1:
+            s = _a.sent();
+
+            if (s && videoRef.current) {
+              stream.current = s;
+              videoRef.current.srcObject = s;
+            }
+
+            return [2
+            /*return*/
+            ];
+        }
+      });
+    });
+  };
 
   var tearDown = function tearDown() {
-    stream === null || stream === void 0 ? void 0 : stream.getTracks().forEach(function (t) {
+    var _a;
+
+    (_a = stream.current) === null || _a === void 0 ? void 0 : _a.getTracks().forEach(function (t) {
       return t.stop();
     });
   };
 
   (0, _react.useEffect)(function () {
-    if (stream) tearDown();
-    navigator.mediaDevices.getUserMedia({
-      audio: false,
-      video: {
-        width: {
-          exact: 128
-        },
-        height: {
-          exact: 128
-        },
-        deviceId: deviceId
-      }
-    }).then(function (s) {
-      return setStream(s);
-    });
+    setUp();
     return tearDown();
   }, [deviceId]);
-  if (videoRef.current && !videoRef.current.srcObject && stream) videoRef.current.srcObject = stream;
   return _react.default.createElement("video", __assign({
     ref: videoRef,
     onCanPlay: function onCanPlay() {
@@ -30984,7 +31149,7 @@ var GameboyCamera = function GameboyCamera() {
             });
 
             if (inputs.length > 0) {
-              setActiveDevice(inputs[1].deviceId);
+              setActiveDevice(inputs[0].deviceId);
               setDevices(inputs);
             }
 
@@ -31050,6 +31215,7 @@ var GameboyCamera = function GameboyCamera() {
       return takePhoto();
     }
   }, "Take Photo"), _react.default.createElement("select", {
+    value: activeDevice,
     onChange: function onChange(e) {
       setActiveDevice(e.target.value);
       console.log(e.target.value);
@@ -31127,7 +31293,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53404" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54349" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
