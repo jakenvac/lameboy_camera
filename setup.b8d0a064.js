6fced7aa23bcefc5cebfba82951b39d3d6b0ba1d
jakehl <jakelangford@outlook.com>
2020-10-18 23:16:40 +0100

    Updates

--- setup.b8d0a064.js
+++ setup.b8d0a064.js
@@ -30600,16 +30600,14 @@ var __rest = void 0 && (void 0).__rest || function (s, e) {
 };
 
 var Camera = _react.default.forwardRef(function (_a, ref) {
-  var facing = _a.facing,
-      props = __rest(_a, ["facing"]);
+  var deviceId = _a.deviceId,
+      props = __rest(_a, ["deviceId"]);
 
   var videoRef = ref !== null && ref !== void 0 ? ref : (0, _react.useRef)();
 
   var _b = (0, _react.useState)(),
       stream = _b[0],
       setStream = _b[1];
-
-  facing = facing !== null && facing !== void 0 ? facing : "user";
 
   var tearDown = function tearDown() {
     stream === null || stream === void 0 ? void 0 : stream.getTracks().forEach(function (t) {
@@ -30618,6 +30616,7 @@ var Camera = _react.default.forwardRef(function (_a, ref) {
   };
 
   (0, _react.useEffect)(function () {
+    if (stream) tearDown();
     navigator.mediaDevices.getUserMedia({
       audio: false,
       video: {
@@ -30627,13 +30626,13 @@ var Camera = _react.default.forwardRef(function (_a, ref) {
         height: {
           exact: 128
         },
-        facingMode: facing
+        deviceId: deviceId
       }
     }).then(function (s) {
       return setStream(s);
     });
     return tearDown();
-  }, []);
+  }, [deviceId]);
   if (videoRef.current && !videoRef.current.srcObject && stream) videoRef.current.srcObject = stream;
   return _react.default.createElement("video", __assign({
     ref: videoRef,
@@ -30794,6 +30793,149 @@ var __makeTemplateObject = void 0 && (void 0).__makeTemplateObject || function (
   return cooked;
 };
 
+var __awaiter = void 0 && (void 0).__awaiter || function (thisArg, _arguments, P, generator) {
+  function adopt(value) {
+    return value instanceof P ? value : new P(function (resolve) {
+      resolve(value);
+    });
+  }
+
+  return new (P || (P = Promise))(function (resolve, reject) {
+    function fulfilled(value) {
+      try {
+        step(generator.next(value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function rejected(value) {
+      try {
+        step(generator["throw"](value));
+      } catch (e) {
+        reject(e);
+      }
+    }
+
+    function step(result) {
+      result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected);
+    }
+
+    step((generator = generator.apply(thisArg, _arguments || [])).next());
+  });
+};
+
+var __generator = void 0 && (void 0).__generator || function (thisArg, body) {
+  var _ = {
+    label: 0,
+    sent: function sent() {
+      if (t[0] & 1) throw t[1];
+      return t[1];
+    },
+    trys: [],
+    ops: []
+  },
+      f,
+      y,
+      t,
+      g;
+  return g = {
+    next: verb(0),
+    "throw": verb(1),
+    "return": verb(2)
+  }, typeof Symbol === "function" && (g[Symbol.iterator] = function () {
+    return this;
+  }), g;
+
+  function verb(n) {
+    return function (v) {
+      return step([n, v]);
+    };
+  }
+
+  function step(op) {
+    if (f) throw new TypeError("Generator is already executing.");
+
+    while (_) {
+      try {
+        if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+        if (y = 0, t) op = [op[0] & 2, t.value];
+
+        switch (op[0]) {
+          case 0:
+          case 1:
+            t = op;
+            break;
+
+          case 4:
+            _.label++;
+            return {
+              value: op[1],
+              done: false
+            };
+
+          case 5:
+            _.label++;
+            y = op[1];
+            op = [0];
+            continue;
+
+          case 7:
+            op = _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+
+          default:
+            if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) {
+              _ = 0;
+              continue;
+            }
+
+            if (op[0] === 3 && (!t || op[1] > t[0] && op[1] < t[3])) {
+              _.label = op[1];
+              break;
+            }
+
+            if (op[0] === 6 && _.label < t[1]) {
+              _.label = t[1];
+              t = op;
+              break;
+            }
+
+            if (t && _.label < t[2]) {
+              _.label = t[2];
+
+              _.ops.push(op);
+
+              break;
+            }
+
+            if (t[2]) _.ops.pop();
+
+            _.trys.pop();
+
+            continue;
+        }
+
+        op = body.call(thisArg, _);
+      } catch (e) {
+        op = [6, e];
+        y = 0;
+      } finally {
+        f = t = 0;
+      }
+    }
+
+    if (op[0] & 5) throw op[1];
+    return {
+      value: op[0] ? op[1] : void 0,
+      done: true
+    };
+  }
+};
+
 var StyledGameboyCamera = _styledComponents.default.div(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  display: flex;\n  flex-direction: column;\n  flex: 1;\n  min-width: 80vmin;\n  @media (min-width: 800px) {\n    min-width: 50vmin;\n  }\n  padding: 20px;\n"], ["\n  display: flex;\n  flex-direction: column;\n  flex: 1;\n  min-width: 80vmin;\n  @media (min-width: 800px) {\n    min-width: 50vmin;\n  }\n  padding: 20px;\n"])));
 
 var StyledH2 = _styledComponents.default.h2(templateObject_2 || (templateObject_2 = __makeTemplateObject(["\n  font-size: 30px;\n  font-family: \"Nunito Sans\", sans-serif;\n  margin: 0;\n  align-text: left;\n  width: 100%;\n  span {\n    color: #fff;\n    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,\n      1px 1px 0 #000;\n    -webkit-text-stroke: 1px black;\n  }\n"], ["\n  font-size: 30px;\n  font-family: \"Nunito Sans\", sans-serif;\n  margin: 0;\n  align-text: left;\n  width: 100%;\n  span {\n    color: #fff;\n    text-shadow: -1px -1px 0 #000, 1px -1px 0 #000, -1px 1px 0 #000,\n      1px 1px 0 #000;\n    -webkit-text-stroke: 1px black;\n  }\n"])));
@@ -30814,11 +30956,46 @@ var GameboyCamera = function GameboyCamera() {
       contrast = _b[0],
       setContrast = _b[1];
 
-  var _c = (0, _react.useState)(true),
-      frontCam = _c[0],
-      setFrontCam = _c[1];
+  var _c = (0, _react.useState)(),
+      deviceIDs = _c[0],
+      setDeviceIDs = _c[1];
+
+  var _d = (0, _react.useState)(undefined),
+      activeDevice = _d[0],
+      setActiveDevice = _d[1];
 
   var interval = 16;
+
+  var updateDevices = function updateDevices() {
+    return __awaiter(void 0, void 0, void 0, function () {
+      var devices, inputs;
+      return __generator(this, function (_a) {
+        switch (_a.label) {
+          case 0:
+            devices = navigator.mediaDevices.enumerateDevices();
+            return [4
+            /*yield*/
+            , devices];
+
+          case 1:
+            inputs = _a.sent().filter(function (d) {
+              return d.kind === "videoinput";
+            });
+
+            if (inputs.length > 0) {
+              setActiveDevice(inputs[0].deviceId);
+              setDeviceIDs(inputs.map(function (d) {
+                return d.deviceId;
+              }));
+            }
+
+            return [2
+            /*return*/
+            ];
+        }
+      });
+    });
+  };
 
   var takePhoto = function takePhoto() {
     var cnvs = canvasRef.current;
@@ -30847,16 +31024,13 @@ var GameboyCamera = function GameboyCamera() {
   };
 
   (0, _react.useEffect)(function () {
+    updateDevices();
     frameTimer();
   }, []);
-  return _react.default.createElement(StyledGameboyCamera, null, _react.default.createElement(StyledH2, null, "LAME BOY ", _react.default.createElement("span", null, "camera")), frontCam ? _react.default.createElement(_camera.default, {
+  return _react.default.createElement(StyledGameboyCamera, null, _react.default.createElement(StyledH2, null, "LAME BOY ", _react.default.createElement("span", null, "camera")), _react.default.createElement(_camera.default, {
     hidden: true,
     ref: videoRef,
-    facing: "user"
-  }) : _react.default.createElement(_camera.default, {
-    hidden: true,
-    ref: videoRef,
-    facing: "environment"
+    deviceId: activeDevice
   }), _react.default.createElement(_filter.default, {
     frame: frame,
     contrast: contrast,
@@ -30876,11 +31050,15 @@ var GameboyCamera = function GameboyCamera() {
     onClick: function onClick() {
       return takePhoto();
     }
-  }, "Take Photo"), _react.default.createElement(StyledButton, {
-    onClick: function onClick() {
-      return setFrontCam(!frontCam);
+  }, "Take Photo"), _react.default.createElement("select", {
+    onChange: function onChange(e) {
+      return setActiveDevice(e.target.value);
     }
-  }, "Swap Cam"));
+  }, deviceIDs && deviceIDs.map(function (d) {
+    return _react.default.createElement("option", {
+      value: d
+    }, d);
+  })));
 };
 
 var _default = GameboyCamera;
@@ -30949,7 +31127,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56556" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52999" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
