a174dd8e292c36107f3088b8f7a5df133c104e22
jakehl <jakelangford@outlook.com>
2020-10-23 03:24:59 +0100

    adds basic pwa support

---
+++ src/sw.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", () => {
+  console.log(`installing service worker`);
+});
+
+self.addEventListener("activate", () => {
+  console.log(`activating service worker`);
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log(`fetching...`);
+});
