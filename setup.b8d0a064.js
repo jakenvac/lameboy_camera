1a00c0364a8db5c8a288455b234d7ead29a423a6
jakehl <jakelangford@outlook.com>
2020-10-23 03:28:39 +0100

    Updates

--- setup.b8d0a064.js
+++ setup.b8d0a064.js
@@ -42617,7 +42617,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61301" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61711" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
